362d0a8885ab4b68af4ceaf321020fb7ec444d76
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 14:37:15 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
   # Add your solution here
+  max_value = array[0]
+
+  array.length.times do |index|
+    if max_value < array[index]
+      max_value = array[index]
+    end
+  end
+  max_value
 end
