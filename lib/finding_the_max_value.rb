09e9e4be8bebf994b8de26914c1bbf9c1569423b
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 14:33:23 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,13 @@
 def find_max_value(array)
   # Add your solution here
+  count = 0
   max = array[0]
-  array.length.times do |index|
+
+  while count < array.length
     if max < array[index]
       max = array[index]
     end
   end
+  
+  
 end
